1fd4f89ca073b7bf34df4837d9d0cedf1b915289
Patrick Veilleux <patrix87@gmail.com>
2022-03-11 17:07:26 -0500

    v3

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 PLAYLISTS = "./playlists.txt"
 
-
 def print_to_console(value: str):
     print(datetime.now().strftime("%Y-%m-%d %H:%M:%S: ") + value)
 
@@ -13,31 +12,36 @@ def parse_playlists() -> list:
     content = open(PLAYLISTS,'r',encoding="utf-8")
     playlists = []
     for line in content:
+        if line[0] == "#": continue
         data = line.split(",")
         playlist = data[0].strip()
-        destination = data[1].strip()
+        source = data[1].strip()
+        destination = data[2].strip()
         try:
             Path(destination).mkdir(parents=True, exist_ok=True)
         except Exception as e:
             print_to_console(f"Error creating directory:\n{e}")
-        playlists.append((playlist,destination))
+        playlists.append((playlist,source,destination))
     return playlists
         
 
-def parse_tracks(playlist: str) -> list:
+def parse_tracks(playlist: str, source: str) -> list:
     content = open(playlist,'r',encoding="utf-8")
     tracks = []
     for line in content:
         if line[0] == "#": continue
-        fullpath = line.strip().replace('"','')
-        trackname = (line.split("/")).pop().replace("'","\'")
+        line = line.strip().replace('"','').replace("'","\'")
+        fullpath = line.split("/")
+        fullpath.pop(0)
+        trackname = fullpath[-1]
+        fullpath = "/".join([source]+fullpath)
         tracks.append((fullpath,trackname))
     return tracks
 
 
 def make_symlink(track: list, destination: str):
     source = track[0]
-    link = f"{destination}{track[1]}"
+    link = f"{destination}/{track[1]}"
     try:
         result = subprocess.run(f'ln -sf "{source}" "{link}"', shell=True, check=True)
         print_to_console(f"Created symlink:\n{result}")
@@ -48,8 +52,9 @@ def make_symlink(track: list, destination: str):
 def main():
     playlists = parse_playlists()
     for playlist in playlists:
-        tracks = parse_tracks(playlist[0])
-        destination = playlist[1]
+        source = playlist[1]
+        destination = playlist[2]
+        tracks = parse_tracks(playlist[0],source)
         for track in tracks:
             make_symlink(track,destination)
 
